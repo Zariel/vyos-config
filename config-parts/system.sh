7f7ebb20a5a5571a36dc55933d8806f2bbda3bd2
Chris Bannister <c.bannister@gmail.com>
2024-04-21 18:18:01 +0100

    ipv6 dns server

--- config-parts/system.sh
+++ config-parts/system.sh
@@ -11,6 +11,7 @@ set system login user vyos authentication public-keys personal key 'AAAAC3NzaC1l
 set system login user vyos authentication public-keys personal type 'ssh-ed25519'
 
 set system name-server '1.1.1.1'
+set system name-server '2606:4700:4700::1111'
 
 set system sysctl parameter kernel.pty.max value '24000'
 
