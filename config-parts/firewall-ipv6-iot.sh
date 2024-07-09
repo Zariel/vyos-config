096ad4ae39e6cbad3eb007ccd1e24d26f010a36e
Chris Bannister <c.bannister@gmail.com>
2024-07-09 10:24:32 +0100

    ipv6: allow mdns between iot<->trusted

--- config-parts/firewall-ipv6-iot.sh
+++ config-parts/firewall-ipv6-iot.sh
@@ -4,10 +4,16 @@
 set firewall ipv6 name iot-wan default-action 'accept'
 
 # From IOT to LOCAL
+set firewall ipv6 name iot-local default-action 'drop'
+set firewall ipv6 name iot-local default-log
 set firewall ipv6 name iot-local rule 20 description 'Rule: allow_icmpv6'
 set firewall ipv6 name iot-local rule 20 action 'accept'
 set firewall ipv6 name iot-local rule 20 protocol 'icmpv6'
-set firewall ipv6 name iot-local default-action 'drop'
+set firewall ipv6 name iot-local rule 110 action 'accept'
+set firewall ipv6 name iot-local rule 110 description 'Rule: accept_mdns'
+set firewall ipv6 name iot-local rule 110 destination port 'mdns'
+set firewall ipv6 name iot-local rule 110 protocol 'udp'
+set firewall ipv6 name iot-local rule 110 source port 'mdns'
 
 # From IOT to CONTAINERS
 set firewall ipv6 name iot-containers default-action 'accept'
