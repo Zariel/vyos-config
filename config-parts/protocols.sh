5b21b312791a18929ae8cff638d2e3eaab610855
Chris Bannister <c.bannister@gmail.com>
2024-11-15 17:54:00 +0000

    bgp: add k8s-3

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -12,5 +12,8 @@ set protocols bgp neighbor 10.1.1.11 remote-as '64512'
 set protocols bgp neighbor 10.1.1.12 address-family ipv4-unicast
 set protocols bgp neighbor 10.1.1.12 description 'k8s-2'
 set protocols bgp neighbor 10.1.1.12 remote-as '64512'
+set protocols bgp neighbor 10.1.1.13 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.1.13 description 'k8s-3'
+set protocols bgp neighbor 10.1.1.13 remote-as '64512'
 set protocols bgp parameters router-id '10.1.0.1'
 set protocols bgp system-as '64512'
