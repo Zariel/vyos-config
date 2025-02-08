e4824f293170737752ffe18df06fbf4e1f5a9ff1
Chris Bannister <c.bannister@gmail.com>
2025-02-08 18:21:25 +0000

    bgp: set remote-as to a different one and use peer-groups

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -3,17 +3,21 @@
 # needed to route traffic to WAN
 set protocols static route 0.0.0.0/0 dhcp-interface eth2
 
-set protocols bgp neighbor 10.1.1.10 address-family ipv4-unicast
+set protocols bgp peer-group k8s graceful-restart enable
+set protocols bgp peer-group k8s remote-as '64513'
+set protocols bgp peer-group k8s address-family ipv4-unicast
+
 set protocols bgp neighbor 10.1.1.10 description 'k8s-0'
-set protocols bgp neighbor 10.1.1.10 remote-as '64512'
-set protocols bgp neighbor 10.1.1.11 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.1.10 peer-group k8s
+
 set protocols bgp neighbor 10.1.1.11 description 'k8s-1'
-set protocols bgp neighbor 10.1.1.11 remote-as '64512'
-set protocols bgp neighbor 10.1.1.12 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.1.11 peer-group k8s
+
 set protocols bgp neighbor 10.1.1.12 description 'k8s-2'
-set protocols bgp neighbor 10.1.1.12 remote-as '64512'
-set protocols bgp neighbor 10.1.1.13 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.1.12 peer-group k8s
+
 set protocols bgp neighbor 10.1.1.13 description 'k8s-3'
-set protocols bgp neighbor 10.1.1.13 remote-as '64512'
+set protocols bgp neighbor 10.1.1.13 peer-group k8s
+
 set protocols bgp parameters router-id '10.1.0.1'
 set protocols bgp system-as '64512'
