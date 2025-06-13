24cfb3466fa7aafb249cbfae40c7cb7f257bb822
Chris Bannister <c.bannister@gmail.com>
2025-06-13 12:47:33 +0100

    interfaces; set 1500 mtu explicitly

--- config-parts/interfaces.sh
+++ config-parts/interfaces.sh
@@ -41,12 +41,15 @@ set interfaces bonding bond0 vif 45 description 'INGRES'
 set interfaces bonding bond0 vif 45 mtu 9000
 set interfaces bonding bond0 vif 20 address '10.1.2.1/24'
 set interfaces bonding bond0 vif 20 description 'TRUSTED'
+set interfaces bonding bond0 vif 20 mtu 1500
 set interfaces bonding bond0 vif 20 ipv6 address eui64 fdc0:7ebe:7a3e:20::/64
 set interfaces bonding bond0 vif 30 address '192.168.2.1/24'
 set interfaces bonding bond0 vif 30 description 'GUEST'
+set interfaces bonding bond0 vif 30 mtu 1500
 set interfaces bonding bond0 vif 40 address '10.1.3.1/24'
 set interfaces bonding bond0 vif 40 ipv6 address eui64 fdc0:7ebe:7a3e:40::/64
 set interfaces bonding bond0 vif 40 description 'IOT'
+set interfaces bonding bond0 vif 40 mtu 1500
 
 set interfaces bonding bond0 member interface eth0
 set interfaces bonding bond0 member interface eth1
