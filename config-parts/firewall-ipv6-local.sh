f0fe7e2bb58625efcb5f50bd781316d4c2e4e716
Chris Bannister <c.bannister@gmail.com>
2025-09-13 10:47:26 +0100

    missed these

--- config-parts/firewall-ipv6-local.sh
+++ config-parts/firewall-ipv6-local.sh
@@ -4,9 +4,6 @@
 set firewall ipv6 name local-wan default-action 'accept'
 
 # From LOCAL to TRUSTED
-set firewall ipv6 name local-trusted rule 20 description 'Rule: allow_icmpv6'
-set firewall ipv6 name local-trusted rule 20 action 'accept'
-set firewall ipv6 name local-trusted rule 20 protocol 'icmpv6'
 set firewall ipv6 name local-trusted rule 30 description 'Rule: allow_dhcpv6'
 set firewall ipv6 name local-trusted rule 30 action 'accept'
 set firewall ipv6 name local-trusted rule 30 destination port '546'
@@ -18,9 +15,6 @@ set firewall ipv6 name local-trusted rule 110 destination port 'mdns'
 set firewall ipv6 name local-trusted rule 110 protocol 'udp'
 
 # From LOCAL to IOT
-set firewall ipv6 name local-iot rule 20 description 'Rule: allow_icmpv6'
-set firewall ipv6 name local-iot rule 20 action 'accept'
-set firewall ipv6 name local-iot rule 20 protocol 'icmpv6'
 set firewall ipv6 name local-iot rule 110 action 'accept'
 set firewall ipv6 name local-iot rule 110 description 'Rule: accept_mdns'
 set firewall ipv6 name local-iot rule 110 destination port 'mdns'
