6f2f74574c08e3c43fcfda5eea397b8ba1110b30
Chris Bannister <c.bannister@gmail.com>
2025-09-24 22:55:01 +0100

    update ospf config

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -19,10 +19,15 @@ set protocols bgp neighbor 10.1.1.12 peer-group k8s
 set protocols bgp parameters router-id '10.1.0.1'
 set protocols bgp system-as '64512'
 
-set protocols ospf interface bond0.5 area 0
-set protocols ospf interface bond0.5 network point-to-point
-set protocols ospf interface bond0.5 dead-interval 40
-set protocols ospf interface bond0.5 hello-interval 10
-set protocols ospf interface bond0.5 priority 1
+set protocols ospf default-information originate always
+set protocols ospf default-information originate metric '1'
+set protocols ospf default-information originate metric-type '2'
+set protocols ospf interface bond0.5 area '0'
+set protocols ospf interface bond0.5 dead-interval '40'
+set protocols ospf interface bond0.5 hello-interval '10'
+set protocols ospf interface bond0.5 network 'point-to-point'
+set protocols ospf interface bond0.5 priority '1'
+set protocols ospf redistribute connected metric '10'
+set protocols ospf redistribute static metric '10'
 set protocols ospf log-adjacency-changes detail
 set protocols ospf parameters router-id '10.1.0.1'
