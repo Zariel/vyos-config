74939c1aa7c3919e5d84e135d6ef35f2835e59ed
Chris Bannister <c.bannister@gmail.com>
2025-10-12 15:51:56 +0100

    fix firewall

--- config-parts/firewall.sh
+++ config-parts/firewall.sh
@@ -52,9 +52,10 @@ set firewall group port-group apple_services_ports port '30000-65535'
 set firewall group port-group roon_arc_ports port '59453'
 
 set firewall group address-group node_exporter_targets address '10.1.2.54' # am6b
-set firewall group address-group node_exporter_targets address '10.1.2.15' # gamiing
+set firewall group address-group node_exporter_targets address '10.1.2.15' # gaming
 set firewall group address-group node_exporter_targets address '10.1.1.31' # pve
 set firewall group address-group node_exporter_targets address '10.1.0.1' # gateway
+set firewall group address-group node_exporter_targets address '10.1.1.155' # builer
 
 set firewall group address-group STORAGE address 10.254.1.101
 
