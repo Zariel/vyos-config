c3b382255b299cc6f41b2aa262cb450a12ccc6d2
Chris Bannister <c.bannister@gmail.com>
2025-10-13 18:56:44 +0100

    update node exporter targets

--- config-parts/firewall.sh
+++ config-parts/firewall.sh
@@ -54,6 +54,7 @@ set firewall group port-group roon_arc_ports port '59453'
 set firewall group address-group node_exporter_targets address '10.1.2.54' # am6b
 set firewall group address-group node_exporter_targets address '10.1.2.15' # gaming
 set firewall group address-group node_exporter_targets address '10.1.1.31' # pve
+set firewall group address-group node_exporter_targets address '10.1.1.41' # pve2
 set firewall group address-group node_exporter_targets address '10.1.0.1' # gateway
 set firewall group address-group node_exporter_targets address '10.1.1.155' # builer
 
