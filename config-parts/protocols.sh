47d9fc29a206f8ac5c2e32c2faea4f5eb0ddc521
Chris Bannister <c.bannister@gmail.com>
2025-10-13 18:56:22 +0100

    no more bgp

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -5,22 +5,6 @@ set protocols static route 0.0.0.0/0 dhcp-interface bond0.99
 
 set protocols static route 10.0.0.0/8 reject
 
-set protocols bgp peer-group k8s graceful-restart enable
-set protocols bgp peer-group k8s remote-as '64513'
-set protocols bgp peer-group k8s address-family ipv4-unicast
-
-set protocols bgp neighbor 10.1.1.10 description 'k8s-0'
-set protocols bgp neighbor 10.1.1.10 peer-group k8s
-
-set protocols bgp neighbor 10.1.1.11 description 'k8s-1'
-set protocols bgp neighbor 10.1.1.11 peer-group k8s
-
-set protocols bgp neighbor 10.1.1.12 description 'k8s-2'
-set protocols bgp neighbor 10.1.1.12 peer-group k8s
-
-set protocols bgp parameters router-id '10.1.0.1'
-set protocols bgp system-as '64512'
-
 set protocols ospf default-information originate always
 set protocols ospf default-information originate metric '1'
 set protocols ospf default-information originate metric-type '2'
