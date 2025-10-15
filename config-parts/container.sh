1223f0be09123269ebca0b3da9cd3fd141a425b8
Chris Bannister <c.bannister@gmail.com>
2025-10-15 18:25:18 +0100

    containers; remove dns stuff

--- config-parts/container.sh
+++ config-parts/container.sh
@@ -15,46 +15,6 @@ set container name haproxy-k8s-api volume config source '/config/containers/hapr
 set container name haproxy-k8s-api volume config destination '/usr/local/etc/haproxy/haproxy.cfg'
 set container name haproxy-k8s-api volume config mode 'ro'
 
-# bind
-set container name bind cap-add 'net-bind-service'
-set container name bind image 'docker.io/internetsystemsconsortium/bind9:9.21'
-set container name bind arguments '-4 -g -c /etc/bind/named.conf'
-set container name bind memory '0'
-set container name bind network containers address '10.5.0.3'
-set container name bind restart 'on-failure'
-set container name bind shared-memory '0'
-set container name bind volume config destination '/etc/bind'
-set container name bind volume config source '/config/containers/bind/config'
-set container name bind volume config mode 'ro'
-# set container name bind volume cache source '/tmp/bind/cache'
-# set container name bind volume cache destination '/var/cache/bind'
-# set container name bind volume cache mode 'rw'
-
-# dnsdist
-set container name dnsdist cap-add 'net-bind-service'
-set container name dnsdist environment TZ value 'Europe/London'
-set container name dnsdist image 'docker.io/powerdns/dnsdist-19:1.9.10'
-set container name dnsdist arguments '--log-timestamps'
-set container name dnsdist memory '0'
-set container name dnsdist network containers address '10.5.0.4'
-set container name dnsdist network containers address 'fd74:f571:d3bd:1005::4'
-set container name dnsdist restart 'on-failure'
-set container name dnsdist shared-memory '0'
-set container name dnsdist volume config source '/config/containers/dnsdist/config/dnsdist.conf.lua'
-set container name dnsdist volume config destination '/etc/dnsdist/dnsdist.conf'
-set container name dnsdist volume config mode 'ro'
-
-# blocky
-set container name blocky image 'ghcr.io/0xerr0r/blocky:v0.26.2'
-set container name blocky memory '0'
-set container name blocky network containers address '10.5.0.7'
-set container name blocky shared-memory '0'
-set container name blocky restart 'on-failure'
-set container name blocky environment TZ value 'Europe/London'
-set container name blocky volume config source '/config/containers/blocky/config/config.yml'
-set container name blocky volume config destination '/app/config.yml'
-set container name blocky volume config mode 'ro'
-
 # node-exporter
 set container name node-exporter environment procfs value '/host/proc'
 set container name node-exporter environment rootfs value '/host/rootfs'
